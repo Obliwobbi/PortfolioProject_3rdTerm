27c086a5fdc736163cc692d0df02a992520a6e2f
Obliwobbi <dev@obli.dk>
2026-04-02 15:11:41 +0200

    initial commit

---
+++ src/main/java/app/config/EntityRegistry.java
@@ -0,0 +1,23 @@
+package app.config;
+
+import app.entities.*;
+import org.hibernate.cfg.Configuration;
+
+final class EntityRegistry
+{
+
+    private EntityRegistry()
+    {
+    }
+
+    static void registerEntities(Configuration configuration)
+    {
+        // TODO: Add entities here...
+//        configuration.addAnnotatedClass(User.class);
+//        configuration.addAnnotatedClass(Company.class);
+//        configuration.addAnnotatedClass(Membership.class);
+//        configuration.addAnnotatedClass(MembershipType.class);
+//        configuration.addAnnotatedClass(Location.class);
+//        configuration.addAnnotatedClass(CheckIn.class);
+    }
+}
