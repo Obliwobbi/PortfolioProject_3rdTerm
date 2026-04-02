416f427038788ae4d635bd2cf9dde49d9b946825
Obliwobbi <dev@obli.dk>
2026-04-02 15:21:59 +0200

    added company entity

---
+++ src/main/java/app/entities/Company.java
@@ -0,0 +1,26 @@
+package app.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "companies", uniqueConstraints = {
+        @UniqueConstraint(name = "uq_company_name", columnNames = "name")
+})
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    // TODO: Add users collection later for bi-directional relationships.
+
+}
