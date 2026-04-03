dd995a6ea7bc31c515c65fc0762160d69dc952cc
Obliwobbi <dev@obli.dk>
2026-04-03 14:55:34 +0200

    created test class for Company Routes and created some minor tests

---
+++ src/test/java/app/rest/CompanyRoutesTest.java
@@ -0,0 +1,115 @@
+package app.rest;
+
+import app.ApplicationConfig;
+import io.javalin.Javalin;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.*;
+
+public class CompanyRoutesTest
+{
+
+    private static Javalin app;
+
+    @BeforeAll
+    static void setUp()
+    {
+        app = ApplicationConfig.startApp(7001);
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 7001;
+    }
+
+    @AfterAll
+    static void tearDown()
+    {
+        if (app != null)
+        {
+            app.stop();
+        }
+    }
+
+    @Test
+    void rootEndpointShouldReturnApiRunningMessage()
+    {
+        RestAssured
+                .given()
+                .when()
+                .get("/")
+                .then()
+                .statusCode(200)
+                .body(org.hamcrest.Matchers.equalTo("MemberSystem API is running"));
+    }
+
+    @Test
+    @DisplayName("Test company name")
+    void createCompany()
+    {
+        String requestBody = """
+            {
+              "name": "Test Company"
+            }
+            """;
+
+        RestAssured
+                .given()
+                    .contentType("application/json")
+                    .body(requestBody)
+                .when()
+                    .post("/companies")
+                .then()
+                    .statusCode(201)
+                    .body("name", org.hamcrest.Matchers.equalTo("Test Company"))
+                    .body("id", org.hamcrest.Matchers.notNullValue());
+    }
+
+    @Test
+    @DisplayName("Get all companies")
+    void getAllCompanies()
+    {
+        RestAssured
+                .given()
+                .when()
+                    .get("/companies")
+                .then()
+                    .statusCode(200);
+    }
+
+    @Test
+    void shouldGetCompanyById() {
+        Long companyId = Integer.toUnsignedLong(
+                RestAssured
+                        .given()
+                        .contentType("application/json")
+                        .body("""
+                              {
+                                "name": "Company For GetById Test"
+                              }
+                              """)
+                        .when()
+                        .post("/companies")
+                        .then()
+                        .statusCode(201)
+                        .extract()
+                        .path("id"));
+
+        RestAssured
+                .given()
+                .when()
+                .get("/companies/" + companyId)
+                .then()
+                .statusCode(200)
+                .body("id", org.hamcrest.Matchers.equalTo(companyId.intValue()))
+                .body("name", org.hamcrest.Matchers.equalTo("Company For GetById Test"));
+    }
+
+    @Test
+    void shouldReturn404WhenCompanyDoesNotExist() {
+        RestAssured
+                .given()
+                .when()
+                .get("/companies/999999")
+                .then()
+                .statusCode(404)
+                .body("status", org.hamcrest.Matchers.equalTo(404));
+    }
+
+}
