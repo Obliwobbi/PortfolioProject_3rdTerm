48e027d5ee84c75fdf792fbce09c6f8b8674ec1a
Obliwobbi <dev@obli.dk>
2026-04-06 13:56:37 +0200

    Created a test hibernate config class to create a test db

---
+++ src/test/java/app/config/HibernateTestConfig.java
@@ -0,0 +1,43 @@
+package app.config;
+
+import jakarta.persistence.EntityManagerFactory;
+
+import java.util.Properties;
+
+public final class HibernateTestConfig
+{
+
+    private static volatile EntityManagerFactory emf;
+
+    private HibernateTestConfig()
+    {
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory()
+    {
+        if (emf == null)
+        {
+            synchronized (HibernateTestConfig.class)
+            {
+                if (emf == null)
+                {
+                    emf = HibernateEmfBuilder.build(buildTestProps());
+                }
+            }
+        }
+        return emf;
+    }
+
+    private static Properties buildTestProps()
+    {
+        Properties props = HibernateBaseProperties.createBase();
+
+        props.put("hibernate.hbm2ddl.auto", "create-drop");
+        props.put("hibernate.connection.url", "jdbc:postgresql://localhost:5432/test_member_system");
+        props.put("hibernate.connection.username", "postgres");
+        props.put("hibernate.connection.password", "postgres");
+
+        return props;
+    }
+
+}
