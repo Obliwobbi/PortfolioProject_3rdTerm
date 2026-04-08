84487e376a2690b1d7fb81cc54032c4e0e030ec0
Obliwobbi <dev@obli.dk>
2026-04-08 02:55:10 +0200

    create account

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,11 +1,61 @@
 package app;
 
 import app.config.HibernateConfig;
+import app.daos.CompanyDAO;
+import app.daos.UserDAO;
+import app.entities.Company;
+import app.entities.Role;
+import app.entities.User;
+import app.services.PasswordService;
+import app.utils.Utils;
+import jakarta.persistence.EntityManagerFactory;
+
+import java.time.LocalDate;
 
 public class Main {
+
     public static void main(String[] args) {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+
+        seedBootstrapAdmin(emf);
+
         int port = getPort();
-        ApplicationConfig.startApp(port, HibernateConfig.getEntityManagerFactory());
+        ApplicationConfig.startApp(port, emf);
+    }
+
+    private static void seedBootstrapAdmin(EntityManagerFactory emf) {
+        UserDAO userDAO = new UserDAO(emf);
+        CompanyDAO companyDAO = new CompanyDAO(emf);
+        PasswordService passwordService = new PasswordService();
+
+        String adminEmail = "admin@obli.dk";
+
+        if (userDAO.findByEmail(adminEmail).isPresent()) {
+            System.out.println("Bootstrap admin already exists.");
+            return;
+        }
+
+        Company company = companyDAO.create(
+                Company.builder()
+                        .name("Membersystem Bootstrap Company")
+                        .build()
+        );
+
+        String password = Utils.getPropertyValue("PASSWORD", "config.properties");
+
+        User admin = User.builder()
+                .company(company)
+                .email(adminEmail)
+                .firstname("Admin")
+                .lastname("User")
+                .dob(LocalDate.of(1990, 1, 1))
+                .role(Role.SYSTEM_ADMIN)
+                .passwordHash(passwordService.hashPassword(password))
+                .build();
+
+        userDAO.create(admin);
+
+        System.out.println("Bootstrap admin created: " + adminEmail);
     }
 
     private static int getPort() {
