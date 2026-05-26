4b1cc4212d819b25d5498a3aa656ba6df609559c
Obliwobbi <dev@obli.dk>
2026-05-26 17:09:24 +0200

    created companyservice interface

---
+++ src/main/java/app/interfaces/ICompanyService.java
@@ -0,0 +1,20 @@
+package app.interfaces;
+
+import app.dto.company.CompanyResponseDTO;
+import app.dto.company.CreateCompanyRequestDTO;
+import app.dto.company.UpdateCompanyRequestDTO;
+
+import java.util.List;
+
+public interface ICompanyService
+{
+    List<CompanyResponseDTO> getAll();
+
+    CompanyResponseDTO getById(Long id);
+
+    CompanyResponseDTO create(CreateCompanyRequestDTO request);
+
+    CompanyResponseDTO update(Long id, UpdateCompanyRequestDTO updateCompanyRequestDTO);
+
+    boolean delete(Long id);
+}
